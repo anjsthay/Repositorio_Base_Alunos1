48e56316a830b77fef2ca92772dbcf8c8d94bbc8
anjsthay <thaylal138@gmail.com>
2026-05-12 09:08:36 -0300

    Add files via upload

---
+++ Modulo 1/Condicionais/temperatura.py
@@ -0,0 +1,10 @@
+temperatura = float(input("Digite a temperatura em Celsius:"))
+                    
+if temperatura >= 30:
+    print("Esta quente!")
+elif temperatura >= 20:
+    print("Está agradável.")
+elif temperatura >= 10:
+    print("Está frio! ")
+else:
+    print("Está muito frio!")
